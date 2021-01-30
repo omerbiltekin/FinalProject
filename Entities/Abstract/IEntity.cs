e3d1d8c14beea2d77d647b5da09674f2da6684e7
Ömer Biltekin <omerbiltekin_@outlook.com>
2021-01-31 00:04:56 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //The IEntity implementing class is a database table
+    public interface IEntity
+    {
+
+    }
+}
