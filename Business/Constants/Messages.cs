be789b2a615fe756996e8cc7845ddfc73a58881b
Ömer Biltekin <omerbiltekin_@outlook.com>
2021-02-11 00:02:22 +0300

    10 February end of lesson

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product added";
+        public static string ProductNameInvalid = "Product name is invalid";
+
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductsListed = "Products listed";
+    }
+}
